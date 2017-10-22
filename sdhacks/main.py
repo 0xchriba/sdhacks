d298271f848bcf0bcb6b6d53ac1ccf877dde6795
patrick4499 <patrick4499@gmail.com>
2017-10-22 01:37:19 -0700

    about

--- sdhacks/main.py
+++ sdhacks/main.py
@@ -1,7 +1,7 @@
 import webapp2
 import os
 import jinja2
-from twilio.rest import Client
+#from twilio.rest import Client
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
